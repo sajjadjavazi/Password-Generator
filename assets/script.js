f534f9f807e7b681d7c90bb5eefeb689103f9b1b
Sajjad <sajjad.javazi.01@gmail.com>
2024-03-22 15:58:11 +0330

    password-generator completed

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,28 @@
-function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-}
+let pass = document.getElementById("pass");
+let btn = document.getElementById("btn");
+let btncp = document.getElementById("btncp");
 
-console.log(generatePassword());
+function generatePass() {
+    let chars =
+        "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
+    let passLength = 20;
+    let password = "";
+
+    for (let i = 0; i < passLength; i++)
+        password += chars[Math.floor(Math.random() * chars.length)];
+
+    return password;
+}
+btn.addEventListener("click", () => {
+    pass.value = generatePass();
+});
+
+btncp.addEventListener("click", () => {
+    if (pass.value != "") {
+        navigator.clipboard.writeText(pass.value);
+        pass.classList.add("active");
+        setTimeout(() => {
+            pass.classList.remove("active");
+        }, 1000);
+    }
+});
